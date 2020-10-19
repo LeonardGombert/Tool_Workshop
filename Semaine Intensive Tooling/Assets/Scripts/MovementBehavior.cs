4379a8d2dd554658ea4c598e96ced7ec9cec244e
Léonard <epoc4oftime@gmail.com>
2020-10-20 00:58:31 +0200

    Constrained field of play

--- Semaine Intensive Tooling/Assets/Scripts/MovementBehavior.cs
+++ Semaine Intensive Tooling/Assets/Scripts/MovementBehavior.cs
@@ -1,26 +1,23 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.InputSystem;
 
 namespace Gameplay.Player
 {
-    public class MovementBehavior : MonoBehaviour
+    public class MovementBehavior : MovementData
     {
-        private InputAction movement;
-        public InputMapping customBindings;
-        public float speed;
-        Vector3 direction;
-
         private void Awake()
         {
             customBindings = new InputMapping();
 
-            movement = customBindings.Player.Move;
-            movement.performed += ctx => Move(ctx);
-            movement.canceled += ctx => Move(ctx);
-        }
+            movementAcion = customBindings.Player.Move;
+            movementAcion.performed += ctx => Move(ctx);
+            movementAcion.canceled += ctx => Move(ctx);
+
+            camera = Camera.main;
+
+            constraints1 = new Vector3(0 + 100, 0 + 100);
+            constraints2 = new Vector3(camera.pixelWidth - 100, camera.pixelHeight - 100);
+    }
 
         void Move(InputAction.CallbackContext context)
         {
@@ -34,7 +31,14 @@ namespace Gameplay.Player
 
         private void Update()
         {
-            transform.position += direction * speed * Time.deltaTime;
+            var movement = direction * movementSpeed * Time.deltaTime;
+            Vector3 screenPos = camera.WorldToScreenPoint(transform.position);
+            Vector3 shiftedPos = camera.WorldToScreenPoint(transform.position + movement);
+            Debug.Log("target is " + screenPos.x + " pixels from the left");
+
+            if (shiftedPos.x > constraints1.x && shiftedPos.y > constraints1.y &&
+                shiftedPos.x < constraints2.x && shiftedPos.y < constraints2.y)
+                transform.position += movement;
         }
 
         private void OnDisable()
---
+++ Semaine Intensive Tooling/Assets/Scripts/MovementData.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MovementData : MonoBehaviour
+{
+    protected InputAction movementAcion;
+    protected InputMapping customBindings;
+    protected Vector3 direction;
+    protected float movementSpeed = 22f;
+
+    protected Camera camera;
+    protected Vector3 constraints1;
+    protected Vector3 constraints2;
+}
