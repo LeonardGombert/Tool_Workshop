48bb94717bf873df480f213ebb499646511178dd
Léonard <epoc4oftime@gmail.com>
2020-10-20 00:25:59 +0200

    Working with Unity Input System

---
+++ Semaine Intensive Tooling/Assets/Scripts/MovementBehavior.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Gameplay.Player
+{
+    public class MovementBehavior : MonoBehaviour
+    {
+        private InputAction movement;
+        public CustomBindings customBindings;
+        public float speed;
+        Vector3 direction;
+
+        private void Awake()
+        {
+            customBindings = new CustomBindings();
+
+            movement = customBindings.Player.Move;
+            movement.performed += ctx => Move(ctx);
+            movement.canceled += ctx => Move(ctx);
+        }
+
+        void Move(InputAction.CallbackContext context)
+        {
+            direction = context.ReadValue<Vector2>();
+        }
+
+        private void OnEnable()
+        {
+            customBindings.Enable();   
+        }
+
+        private void Update()
+        {
+            transform.position += direction * speed * Time.deltaTime;
+        }
+
+        private void OnDisable()
+        {
+            customBindings.Disable();
+        }
+    }
+}
