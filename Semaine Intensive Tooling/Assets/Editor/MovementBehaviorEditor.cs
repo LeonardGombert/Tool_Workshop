4c0125e58fcc3e1fd2aea84f57266fa6b3925b71
Léonard <epoc4oftime@gmail.com>
2020-10-20 18:55:30 +0200

    Starting Playspace editor window

---
+++ Semaine Intensive Tooling/Assets/Editor/MovementBehaviorEditor.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEditor;
+using Gameplay.Player;
+
+[CustomEditor(typeof(MovementBehavior))]
+public class MovementBehaviorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        if (GUILayout.Button("Open Playspace Editor")) PlaySpaceEditorWindow.InitWithContent(target as MovementBehavior);
+    }
+}
---
+++ Semaine Intensive Tooling/Assets/Editor/PlaySpaceEditorWindow.cs
@@ -0,0 +1,23 @@
+﻿using Gameplay.Player;
+using UnityEditor;
+using UnityEngine;
+
+public class PlaySpaceEditorWindow : EditorWindow
+{
+    MovementBehavior movementBehavior;
+
+    [MenuItem("Window/Playspace Editor Window %w")]
+    public static void Init()
+    {
+        PlaySpaceEditorWindow window = EditorWindow.GetWindow(typeof(PlaySpaceEditorWindow)) as PlaySpaceEditorWindow;
+        window.Show();
+
+    }
+
+    public static void InitWithContent(MovementBehavior _movementBehavior)
+    {
+        PlaySpaceEditorWindow window = EditorWindow.GetWindow(typeof(PlaySpaceEditorWindow)) as PlaySpaceEditorWindow;
+        window.movementBehavior = _movementBehavior;
+        window.Show();
+    }
+}
