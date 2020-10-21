4a0882c0ee08860467e256f0768fe5bdba1cd569
Léonard <epoc4oftime@gmail.com>
2020-10-21 10:16:47 +0200

    Rename Project

---
+++ Proje_Tooling/Assets/Editor/MovementBehaviorEditor.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEditor;
+using Gameplay.Player;
+
+[CustomEditor(typeof(MovementBehavior))]
+public class MovementBehaviorEditor : Editor
+{
+    MovementBehavior movementBehavior;
+
+    SerializedProperty playerSpeed;
+    SerializedProperty vectorBounds;
+
+    bool foldoutState;
+
+    private void OnEnable()
+    {
+        movementBehavior = target as MovementBehavior;
+        playerSpeed = serializedObject.FindProperty(nameof(movementBehavior.movementSpeed));
+        vectorBounds = serializedObject.FindProperty(nameof(movementBehavior.playSpace));
+    }
+
+    public override void OnInspectorGUI()
+    {
+        if (GUILayout.Button("Open Playspace Editor")) PlaySpaceEditorWindow.InitWithContent(target as MovementBehavior);
+
+        serializedObject.Update();
+        EditorGUILayout.BeginVertical();
+
+        EditorGUILayout.PropertyField(playerSpeed);
+        EditorGUILayout.PropertyField(vectorBounds);
+
+        foldoutState = EditorGUILayout.Foldout(foldoutState, "Vector4 Bounds", true);
+
+        if(foldoutState)
+        {
+            EditorGUILayout.BeginHorizontal();
+            //GUILayout.Label("Vector4 Bounds");
+            EditorGUILayout.PropertyField(vectorBounds.FindPropertyRelative("rightX"), GUIContent.none);
+            EditorGUILayout.PropertyField(vectorBounds.FindPropertyRelative("rightY"), GUIContent.none);
+            EditorGUILayout.PropertyField(vectorBounds.FindPropertyRelative("leftX"), GUIContent.none);
+            EditorGUILayout.PropertyField(vectorBounds.FindPropertyRelative("leftY"), GUIContent.none);
+            EditorGUILayout.EndHorizontal();
+        }
+
+        EditorGUILayout.EndVertical();
+        serializedObject.ApplyModifiedProperties();
+        
+    }
+}
---
+++ Proje_Tooling/Assets/Scripts/Data/MovementData.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Gameplay.Player
+{
+    public class MovementData : MonoBehaviour
+    {
+        [HideInInspector] public Vector3 moveDirection;
+        [HideInInspector] public float movementSpeed = 22f;
+
+        protected Camera camera;
+        [HideInInspector] public Vector4Bounds playSpace = new Vector4Bounds();
+    }
+}
+
+[System.Serializable]
+public struct Vector4Bounds
+{
+    public float leftX, leftY, rightX, rightY;
+}
