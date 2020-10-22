1c8edf3e9e0986abc116aaccd9cdef1eb2d1bf64
Léonard <epoc4oftime@gmail.com>
2020-10-22 18:12:50 +0200

    Spawn Playspace change zone

--- Projet_Tooling/Assets/Editor/LevelTrackEditorWindow.cs
+++ Projet_Tooling/Assets/Editor/LevelTrackEditorWindow.cs
@@ -9,10 +9,11 @@ public class LevelTrackEditorWIndow : EditorWindow
 {
     List<int> obstacleRectsIndexes = new List<int>();
     List<Vector2> obstacleRectCoords = new List<Vector2>();
-    List<Vector2> convertedRectCoords = new List<Vector2>();
     List<GameObject> obstaclesList = new List<GameObject>();
 
     List<int> playSpaceRectsIndexes = new List<int>();
+    List<Vector2> playspaceRectCoords = new List<Vector2>();
+    List<GameObject> playspaceList = new List<GameObject>();
 
     // grid paramaters
     int tunnelColumns;
@@ -32,6 +33,7 @@ public class LevelTrackEditorWIndow : EditorWindow
     Brush myBrush;
 
     GameObject obstaclePrefab;
+    GameObject playspaceChangePrefab;
 
     [MenuItem("Window/Level Track Editor Window %k")]
     public static void InitWithContent()
@@ -48,13 +50,14 @@ public class LevelTrackEditorWIndow : EditorWindow
 
         #region Enum Brush GUI
         EditorGUILayout.BeginHorizontal();
-        EditorGUILayout.LabelField("Current Brush");
+        //EditorGUILayout.LabelField("Current Brush");
         myBrush = (Brush)EditorGUILayout.EnumPopup(myBrush);
         //if (cur.type == EventType.ScrollWheel && myEnum>0 && myEnum < Brush.obstacle + 1) myEnum += (int)Mathf.Sign(cur.delta.y);
         #endregion
 
         #region PropertyFields GUI
-        obstaclePrefab = EditorGUILayout.ObjectField(obstaclePrefab, typeof(GameObject), true) as GameObject;
+        obstaclePrefab = EditorGUILayout.ObjectField("Obstacle", obstaclePrefab, typeof(GameObject), true) as GameObject;
+        playspaceChangePrefab = EditorGUILayout.ObjectField("Playspace Change", playspaceChangePrefab, typeof(GameObject), true) as GameObject;
         #endregion
 
         #region Draw All Obstacles in Scene GUI
@@ -64,10 +67,19 @@ public class LevelTrackEditorWIndow : EditorWindow
             foreach (Vector2 item in obstacleRectCoords)
             {
                 Vector3 convertedCoords = ScaleToWorldSpace(item);
-                GameObject newObstacle = 
-                    Instantiate(obstaclePrefab, 
+                GameObject newObstacle =
+                    Instantiate(obstaclePrefab,
                     convertedCoords, Quaternion.identity, GameObject.Find("MOVING OBJECTS").transform);
                 obstaclesList.Add(newObstacle);
+            }
+
+            foreach (Vector2 item in playspaceRectCoords)
+            {
+                Vector3 convertedCoords = ScaleToWorldSpace(item);
+                GameObject newPlaySpaceChange =
+                    Instantiate(playspaceChangePrefab,
+                    convertedCoords, Quaternion.identity, GameObject.Find("MOVING OBJECTS").transform);
+                playspaceList.Add(newPlaySpaceChange);
             }
         }
 
@@ -109,23 +121,33 @@ public class LevelTrackEditorWIndow : EditorWindow
                 if (myBrush == Brush.playSpaceTransition)
                 {
                     // if the player right clicks on an already green tile -> delete
-                    if (newRect.Contains(cur.mousePosition) && cur.type == EventType.MouseDown && playSpaceRectsIndexes.Contains(i) && cur.button == 1) 
+                    if (newRect.Contains(cur.mousePosition) && cur.type == EventType.MouseDown && playSpaceRectsIndexes.Contains(i) && cur.button == 1)
+                    {
                         playSpaceRectsIndexes.Remove(i);
+                        playspaceRectCoords.Remove(new Vector2(z, y));
+                    }
 
                     // if the player clicks on a tile that isn't green -> turn it green
                     else if (newRect.Contains(cur.mousePosition) && cur.type == EventType.MouseDown && !playSpaceRectsIndexes.Contains(i))
                     {
                         // if the player is clicking on a tile at the bottom of the screen
-                        if ((i % tunnelColumns) == 0) 
-                            for (int j = i; j >= i - (tunnelColumns - 1); j--) 
+                        if ((i % tunnelColumns) == 0)
+                        {
+                            playspaceRectCoords.Add(new Vector2(z, y));
+                            for (int j = i; j >= i - (tunnelColumns - 1); j--)
                                 playSpaceRectsIndexes.Add(j);
+                        }
+
 
                         // else, iterate through the tiles until you find the bottom one, and draw them back to the top
                         else
-                            for (int l = i; l < i + tunnelColumns; l++) 
-                                if (l % tunnelColumns == 0) 
-                                    for (int j = l; j >= l - (tunnelColumns - 1); j--) 
+                            for (int l = i; l < i + tunnelColumns; l++)
+                                if (l % tunnelColumns == 0)
+                                {
+                                    playspaceRectCoords.Add(new Vector2(z, y));
+                                    for (int j = l; j >= l - (tunnelColumns - 1); j--)
                                         playSpaceRectsIndexes.Add(j);
+                                }
                     }
                 }
 
