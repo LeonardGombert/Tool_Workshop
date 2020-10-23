0648daf93d1a6eeee0bb424ebf0c8b72acdade1e
Léonard <epoc4oftime@gmail.com>
2020-10-23 21:04:19 +0200

    Added post processing stack

---
+++ Projet_Tooling/Assets/Scripts/Behaviors/PostProcessVignetting.cs
@@ -0,0 +1,24 @@
+﻿using Gameplay.Player;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class PostProcessVignetting : MonoBehaviour
+{
+    private Vignette vignette;
+    [SerializeField] MovementBehavior player;
+    [SerializeField] PostProcessVolume postProcVolume;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        postProcVolume.profile.TryGetSettings(out vignette);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CustomScaler.Scale(vignette.intensity, player.playspace.leftX, 0, 0, 1);
+    }
+}
